f1cc7a17a80403083581e6c30a4802f6cda3a7d8
Gabriel <gabrielsolanodesixto@gmail.com>
2023-02-13 10:42:27 -0600

    carrito

--- js/app.js
+++ js/app.js
@@ -1,44 +1,56 @@
-const CarroCompras = document.getElementById('productosCarritos')
+const Clickbutton = document.querySelectorAll('.button')
+const table = document.querySelector('.tbody')
+let carrito = []
+
+Clickbutton.forEach(btn => {
+  btn.addEventListener('click', addToCarritoItem)
+})
 
 
-const productos =[
-    {id: 1, nombre: "Tacos al pastor", precio: 5, stock:10, img : "../img/Tacos_Pastor.jpg"},
-    {id: 2, nombre: "Tacos de carne asada", precio: 5, stock:10, img : "../img/Tacos_Asada.jpg"},
-    {id: 3, nombre: "Tacos dorados", precio: 5, stock:10, img : "../img/Tacos_Dorados.jpg"},
-    {id: 4, nombre: "Pizza Peperoni", precio: 5, stock:10, img : "../img/Pperoni.jpg"},
-    {id: 5, nombre: "Pizza Rellena", precio: 5, stock:10, img : "../img/Prqueso.jpg"},
-    {id: 6, nombre: "Pizza Hawai", precio: 5, stock:10, img : "../img/Phawai.jpg"}
-]
+function addToCarritoItem(e){
+  const button = e.target
+  const item = button.closest('.contenedor')
+  const itemTitle = item.querySelector('.Title').textContent;
+  const itemPrice = item.querySelector('.precio').textContent;
+  const itemImg = item.querySelector('.imagen').src;
+  
+  const newItem = {
+    title: itemTitle,
+    precio: itemPrice,
+    img: itemImg,
+    cantidad: 1
+  }
 
-const Carrito = []
-
-const addCarrito = (id,carrito)=>{
-    const product = productos.find(item => item.id == id);
-    carrito.push(product);
-    alert("Se agrego al carrito", carrito);
+  addItemCarrito(newItem)
 }
 
 
-productos.forEach(almacen =>{
-    const div = document.createElement('div');
-    div.innerHTML += `
-    <div class="card cardComidas">
-        <img src="${almacen.img}" class="card-img-top imgComidas" alt="${almacen.nombre}">
-        <div class="card-body cuerpoCard">
-            <h3 class="card-title tituloComida">${almacen.nombre}</h3>
-            <p class="card-text precioComida">Precio: $${almacen.precio} </p>
-            <button class="btn btnAgregarCarrito" id="${almacen.id}">AGREGAR AL CARRITO</button>
-        </div>
-    </div>
+function addItemCarrito(newItem){
+      
+  carrito.push(newItem) 
+  renderCarrito()
+} 
+
+
+function renderCarrito(){
+  table.innerHTML = ''
+  carrito.map(item => {
+    const tr = document.createElement('tr')
+    tr.classList.add('ItemCarrito')
+    const Content = `
+    <tr>
+    <td><img src=${item.img}  class="w-40" alt=""></td>
+    <td> ${item.title}</td>
+    <td><center> $ ${item.precio}</center></td>
+    <td> <input type="number" min="1" value=${item.cantidad} class="input__elemento"></td>
+    <td> <button class="delete btn btn-danger">x</button></td>
+  </tr>  
     `
-    CarroCompras.appendChild(div);
+    tr.innerHTML = Content;
+    table.append(tr);
 
-})
+   
+  })
 
-const botones = document.querySelectorAll('.btnAgregarCarrito');
-botones.forEach(boton => {
-  boton.addEventListener('click', e => {
-    addCarrito(e.target.id, Carrito);
-  });
-});
+}
 
